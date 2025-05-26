3c2c232dd2c8fc6729a3be62cd94befadf0e77e2
Yusang-1 <206505633+Yusang-1@users.noreply.github.com>
2025-05-26 20:35:03 +0900

    feat : 플레이어를 가리는 오브젝트 투명 기능
    - 플레이어를 가리는 오브젝트를 투명하게 하는 기능입니다.

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+
+
+
+    void LateUpdate()
+    {
+        Vector3 direction = (player.transform.position - transform.position).normalized;
+        RaycastHit[] hits = Physics.RaycastAll(transform.position, direction, Mathf.Infinity, 1 << 6);
+
+        if (hits == null) return;
+
+        List<TransparentObject> obj = new List<TransparentObject>();
+
+        for (int i = 0; i < hits.Length; i++)
+        {
+            obj.Add(hits[i].transform.GetComponent<TransparentObject>());
+            obj[i]?.BecomeTransparent();
+        }
+    }
+}
---
+++ Assets/Scripts/TransparentObject.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class TransparentObject : MonoBehaviour
+{   
+    MeshRenderer meshRenderer;
+    int iNum;
+    void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    private void Update()
+    {
+        iNum = 1;
+
+        if(iNum == 1)
+        {
+            NoTransparent();
+        }
+    }
+
+    public void BecomeTransparent()
+    {
+        iNum = 0;
+        meshRenderer.material.color = new Color(meshRenderer.material.color.r, meshRenderer.material.color.g, meshRenderer.material.color.b, 0);
+    }
+
+    public void NoTransparent()
+    {
+        meshRenderer.material.color = new Color(meshRenderer.material.color.r, meshRenderer.material.color.g, meshRenderer.material.color.b, 1);
+    }
+}
