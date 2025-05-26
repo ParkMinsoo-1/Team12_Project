08621e1171b79306ac88d721af3e82ba9c537ab0
ParkMinsoo <harimaho88@gmail.com>
2025-05-26 21:34:20 +0900

    PlayerController : Move

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,18 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Rigidbody _rigidbody;
+    private Vector2 movInput;
+    
+    [Header ("Movement")]
+    [SerializeField] private float movSpeed;
+
+    private void Awake()
     {
-        
+        _rigidbody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
+        Move();
+    }
+    
+    public void MoveInput(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Performed)
+        {
+            movInput = context.ReadValue<Vector2>();
+        }
+        else if (context.phase == InputActionPhase.Canceled)
+        {
+            movInput = Vector2.zero;
+        }
+    }
+
+    void Move()
+    {
+        Vector3 forward = Vector3.forward;
+        Vector3 right = Vector3.right;
         
+        Vector3 direction = forward*movInput.y + right*movInput.x;
+        direction *= movSpeed;
+        direction.y = _rigidbody.velocity.y;
+        _rigidbody.velocity = direction;
     }
 }
