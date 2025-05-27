e1ea6e789ee2c5c81f16204f3f75f48866369f07
Yusang-1 <206505633+Yusang-1@users.noreply.github.com>
2025-05-27 20:23:17 +0900

    feat : 아이템 랜덤 생성 기능
    - 아이템 랜덤 생성 기능입니다.

---
+++ Assets/Scripts/SpwanItem.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpwanItem : MonoBehaviour
+{
+    [SerializeField] GameObject Item1;
+
+    Transform[] spawnAreaArr;
+
+    private void Start()
+    {
+        spawnAreaArr = GetComponentsInChildren<Transform>();
+        SpawnItem();
+    }
+
+    void SpawnItem()
+    {
+        for(int j = 1; j < spawnAreaArr.Length; j++)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                float x = Random.Range(0, spawnAreaArr[j].localScale.x) / spawnAreaArr[j].localScale.x - 0.5f;
+                float z = Random.Range(0, spawnAreaArr[j].localScale.z) / spawnAreaArr[j].localScale.z - 0.5f;
+
+                Vector3 spawnVec = new Vector3(x, 0, z);
+                Vector3 spawnScale = new Vector3(1 / spawnAreaArr[j].localScale.x, 1, 1 / spawnAreaArr[j].localScale.z);
+
+                GameObject go = Instantiate(Item1, spawnAreaArr[j]);
+                go.transform.localPosition = spawnVec;
+                go.transform.localScale = spawnScale;
+            }
+        }        
+    }
+}
