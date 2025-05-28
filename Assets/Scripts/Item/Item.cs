8d06550dc9e96da23b3d243f3d739a6b45cfa32f
Yamatchi <ussousso12@gmail.com>
2025-05-29 00:31:18 +0900

    feat : anim 추가
    WorkBench_Anvil anim
    Tree
    Stone

--- Assets/Scripts/Item/Item.cs
+++ Assets/Scripts/Item/Item.cs
@@ -4,4 +4,10 @@ public class Item : MonoBehaviour
 {
     [SerializeField] private ItemDataSO itemData;
     public ItemDataSO ItemData => itemData;
+
+    [SerializeField] private float itemRotate;
+    private void Update()
+    {
+        transform.Rotate(Vector3.up * itemRotate * Time.deltaTime);
+    }
 }
