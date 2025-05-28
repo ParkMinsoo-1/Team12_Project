443ccc112d94bc5e1e0dc716ce47ed567fab9bfc
ParkMinsoo <harimaho88@gmail.com>
2025-05-28 22:39:46 +0900

    add : Crafting

--- Assets/Scripts/Craft/Crafting.cs
+++ Assets/Scripts/Craft/Crafting.cs
@@ -4,5 +4,21 @@ using UnityEngine;
 
 public class Crafting : MonoBehaviour
 {
-    
+    public InventoryManager playerInventory;
+
+    public bool Craft(Recipe recipe)
+    {
+        if (playerInventory.HasResource(recipe.resources))
+        {
+            playerInventory.RemoveResource(recipe.resources);
+            playerInventory.PickUp(recipe.result);
+            Debug.Log(recipe.result.itemName + "을(를) 제작했습니다!");
+            return true;
+        }
+        else
+        {
+            Debug.Log("재료가 부족합니다!");
+            return false;
+        }
+    }
 }
--- Assets/Scripts/InventoryManager.cs
+++ Assets/Scripts/InventoryManager.cs
@@ -26,6 +26,7 @@ public class InventoryManager : MonoBehaviour
     private TestInventory Inventory => inventory;
     #endregion
     [SerializeField] private List<ItemDataSO> playerInven = new List<ItemDataSO>();
+    
 
 
 
@@ -56,5 +57,24 @@ public class InventoryManager : MonoBehaviour
         
 
         //Debug.Log($"[�κ��丮] {itemData.itemName}");
+    }
+    
+    
+    public bool HasResource(List<ItemDataSO> requiredItems)
+    {
+        foreach (var requiredItem in requiredItems)
+        {
+            if (!playerInven.Contains(requiredItem))
+                return false;
+        }
+        return true;
+    }
+
+    public void RemoveResource(List<ItemDataSO> usedItems)
+    {
+        foreach (var usedItem in usedItems)
+        {
+            playerInven.Remove(usedItem);
+        }
     }
 }
