ad1bb51354c81aad89c115905ac690e987cb86c3
Yusang-1 <206505633+Yusang-1@users.noreply.github.com>
2025-05-28 11:15:11 +0900

    feat : 위치에 따라 다른 가구 랜덤 생성

--- Assets/Scripts/SpawnFurniture.cs
+++ Assets/Scripts/SpawnFurniture.cs
@@ -1,15 +1,27 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnFurniture : MonoBehaviour
 {
-    [SerializeField] GameObject Item1;
-
+    [SerializeField] GameObject Item0;
+    [SerializeField] GameObject Item2;
+    List<GameObject> Items = new List<GameObject>();
     Transform[] spawnAreaArr;
+    int[] SpawnType;
     Quaternion furnQuaternion;
 
     void Start()
     {
         spawnAreaArr = GetComponentsInChildren<Transform>();
+        Items.Add(Item0);
+        Items.Add(Item2);
+        Items.Add(Item2);
+        SpawnType = new int[spawnAreaArr.Length];
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            SpawnType[i+1] = int.Parse(transform.GetChild(i).name);
+        }
+
         furnQuaternion = Quaternion.identity;
         SpawnFurns();
     }
@@ -20,13 +32,42 @@ public class SpawnFurniture : MonoBehaviour
         {
             float x = Random.Range(0, spawnAreaArr[j].localScale.x) / spawnAreaArr[j].localScale.x - 0.5f;
             float z = Random.Range(0, spawnAreaArr[j].localScale.z) / spawnAreaArr[j].localScale.z - 0.5f;
-
+            int type = SpawnType[j] / 10;
+            int iNum = GetType(type);
+            int rotation = GetRotation(SpawnType[j] % 10);
+            
             Vector3 spawnVec = new Vector3(x, 0, z);
             //Vector3 spawnScale = new Vector3(1 / spawnAreaArr[j].localScale.x, 1, 1 / spawnAreaArr[j].localScale.z);
 
-            GameObject go = Instantiate(Item1, Vector3.zero, furnQuaternion, spawnAreaArr[j]);
+            GameObject go = Instantiate(Items[type], Vector3.zero, furnQuaternion, spawnAreaArr[j]);
+            go.transform.GetChild(iNum).gameObject.SetActive(true);
             go.transform.localPosition = spawnVec;
-            go.transform.rotation = Quaternion.Euler(new Vector3(0, 90, 0));
+            go.transform.rotation = Quaternion.Euler(new Vector3(0, rotation, 0));
         }
+    }
+
+    int GetRotation(int rotation)
+    {
+        switch (rotation)
+        {
+            case 1:
+                return 270;
+            case 2:
+                return 0;
+            case 3:
+                return 90;
+            case 4:
+                return 180;
+            default:
+                return 0;
+        }
+    }
+
+    int GetType(int type)
+    {
+        int count = Items[type].transform.childCount;
+        int iNum = Random.Range(0, count);
+
+        return iNum;
     }
 }
