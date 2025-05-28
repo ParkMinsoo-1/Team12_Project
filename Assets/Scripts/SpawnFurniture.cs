250d47ef48e07306d6b4a209ce424d2287265d12
Yusang-1 <206505633+Yusang-1@users.noreply.github.com>
2025-05-28 12:46:01 +0900

    chore : 프리펩화, 가구 생성

--- Assets/Scripts/SpawnFurniture.cs
+++ Assets/Scripts/SpawnFurniture.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 public class SpawnFurniture : MonoBehaviour
 {
     [SerializeField] GameObject Item0;
+    [SerializeField] GameObject Item1;
     [SerializeField] GameObject Item2;
+    [SerializeField] GameObject Item3;
     List<GameObject> Items = new List<GameObject>();
     Transform[] spawnAreaArr;
     int[] SpawnType;
@@ -14,8 +16,9 @@ public class SpawnFurniture : MonoBehaviour
     {
         spawnAreaArr = GetComponentsInChildren<Transform>();
         Items.Add(Item0);
+        Items.Add(Item1);
         Items.Add(Item2);
-        Items.Add(Item2);
+        Items.Add(Item3);
         SpawnType = new int[spawnAreaArr.Length];
         for (int i = 0; i < transform.childCount; i++)
         {
@@ -30,14 +33,13 @@ public class SpawnFurniture : MonoBehaviour
     {
         for (int j = 1; j < spawnAreaArr.Length; j++)
         {
-            float x = Random.Range(0, spawnAreaArr[j].localScale.x) / spawnAreaArr[j].localScale.x - 0.5f;
-            float z = Random.Range(0, spawnAreaArr[j].localScale.z) / spawnAreaArr[j].localScale.z - 0.5f;
+            float x = Random.Range(-0.5f, 0.5f);
+            float z = Random.Range(-0.5f, 0.5f);
             int type = SpawnType[j] / 10;
             int iNum = GetType(type);
             int rotation = GetRotation(SpawnType[j] % 10);
             
             Vector3 spawnVec = new Vector3(x, 0, z);
-            //Vector3 spawnScale = new Vector3(1 / spawnAreaArr[j].localScale.x, 1, 1 / spawnAreaArr[j].localScale.z);
 
             GameObject go = Instantiate(Items[type], Vector3.zero, furnQuaternion, spawnAreaArr[j]);
             go.transform.GetChild(iNum).gameObject.SetActive(true);
