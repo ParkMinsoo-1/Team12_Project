f8564c74ffe0a83acd5b2d545a7bb91718e3cf09
ParkMinsoo <harimaho88@gmail.com>
2025-05-28 13:05:59 +0900

    edit : PlayerStatus

--- Assets/Scripts/Player/StatusManager.cs
+++ Assets/Scripts/Player/StatusManager.cs
@@ -10,6 +10,6 @@ public class StatusManager : MonoBehaviour
 
     private void Start()
     {
-        PlayerManager.Instance.Player.
+        PlayerManager.Instance.Player._Status.statusManager = this;
     }
 }
