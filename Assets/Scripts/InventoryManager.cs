2cdfafcd251c78c71aa0c73e47d782d7204fc30f
Yusang-1 <206505633+Yusang-1@users.noreply.github.com>
2025-05-28 23:36:56 +0900

    chore : 건설기능 관련 InventoryManager 수정

--- Assets/Scripts/InventoryManager.cs
+++ Assets/Scripts/InventoryManager.cs
@@ -1,4 +1,6 @@
 using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
 using UnityEngine;
 
 public class InventoryManager : MonoBehaviour
@@ -7,8 +9,8 @@ public class InventoryManager : MonoBehaviour
     private static InventoryManager instance;
     public static InventoryManager Instance
     {
-        get 
-        { 
+        get
+        {
             if (instance == null)
             {
                 GameObject go = new GameObject("InventoryManager");
@@ -53,8 +55,52 @@ public class InventoryManager : MonoBehaviour
         //Controller player = FindObjectOfType<Controller>();
         playerInven.Add(itemData);
         Debug.Log($"인벤토리에 {itemData.itemName} 이 추가되었습니다.");
-        
+
 
         //Debug.Log($"[�κ��丮] {itemData.itemName}");
+    }
+
+    //public void SpendResource(string name1, string name2, int lv, Dictionary<string, int>[] UpgradeRequirement)
+    //{
+    //    int woodCount = playerInven.Where(x => x.itemName == name1).Count();
+    //    int stoneCount = playerInven.Where(x => x.itemName == name2).Count();
+
+    //    if (woodCount >= UpgradeRequirement[lv][name1] && stoneCount >= UpgradeRequirement[lv][name2])
+    //    {
+    //        playerInven.Remove(playerInven.Where(x => x.itemName == "Wood").Last());
+    //        playerInven.Remove(playerInven.Where(x => x.itemName == "Stone").Last());
+    //    }
+    //    else
+    //    {
+    //        Debug.Log("자원부족");
+    //    }
+    //}
+
+    public void SpendResource(string[] resourceName, int[] resourceCount)
+    {
+        int length = resourceName.Length;
+        int[] resourcePlayerHaveCnt = new int[length];
+        bool isResourceEnough = true;
+
+        for (int i = 0; i < length; i++)
+        {
+            resourcePlayerHaveCnt[i] = playerInven.Where(x => x.itemName == resourceName[i]).Count();
+            if (resourcePlayerHaveCnt[i] >= resourceCount[i] == false)
+            {
+                isResourceEnough = false;
+            }
+        }
+
+        if (isResourceEnough == true)
+        {
+            for (int i = 0; i < length; i++)
+            {
+                playerInven.Remove(playerInven.Where(x => x.itemName == resourceName[i]).Last());
+            }
+        }
+        else
+        {
+            Debug.Log("자원부족");
+        }
     }
 }
