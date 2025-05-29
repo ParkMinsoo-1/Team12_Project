6298c0c2c269a35ef8f8ae254571ad10ba92760f
Yamatchi <ussousso12@gmail.com>
2025-05-29 19:24:04 +0900

    chore : 주석 해제
    개인적으로 주석처리했던 코드 해제

--- Assets/Scripts/ReadBuildData.cs
+++ Assets/Scripts/ReadBuildData.cs
@@ -1,30 +1,30 @@
-//using UnityEngine;
-//using Newtonsoft.Json;
-//using System.IO;
+using UnityEngine;
+using Newtonsoft.Json;
+using System.IO;
 
-//public class ReadBuildData : MonoBehaviour
-//{
-//    BuildableObjectsData[][] buildableObjectsData;
-//    void ReadData()
-//    {
-//        string ReadString;
-//        string filePath = Application.dataPath + "/json/BuildResourcesData.json";
-//        try
-//        {
-//            ReadString = File.ReadAllText(filePath);
-//        }
-//        catch
-//        {
-//            Debug.LogError("json 파일 오류");
-//            ReadString = default;
-//        }
+public class ReadBuildData : MonoBehaviour
+{
+    BuildableObjectsData[][] buildableObjectsData;
+    void ReadData()
+    {
+        string ReadString;
+        string filePath = Application.dataPath + "/json/BuildResourcesData.json";
+        try
+        {
+            ReadString = File.ReadAllText(filePath);
+        }
+        catch
+        {
+            Debug.LogError("json 파일 오류");
+            ReadString = default;
+        }
 
-//        buildableObjectsData = JsonConvert.DeserializeObject<BuildableObjectsData[][]>(ReadString);
-//    }
-    
-//    public BuildableObjectsData[][] GetReadData()
-//    {
-//        ReadData();
-//        return buildableObjectsData;
-//    }
-//}
+        buildableObjectsData = JsonConvert.DeserializeObject<BuildableObjectsData[][]>(ReadString);
+    }
+
+    public BuildableObjectsData[][] GetReadData()
+    {
+        ReadData();
+        return buildableObjectsData;
+    }
+}
