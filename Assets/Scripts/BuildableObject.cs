6bc83e42a7447c5bf45ed6180a48da720f0e4f8e
Yusang-1 <206505633+Yusang-1@users.noreply.github.com>
2025-05-29 19:13:44 +0900

    chore : chore

--- Assets/Scripts/BuildableObject.cs
+++ Assets/Scripts/BuildableObject.cs
@@ -1,24 +1,34 @@
 ﻿using UnityEngine;
 
+enum Builderable
+{
+    Builderable1 = 0,
+    Builderable2,
+    Builderable3,
+    Tent
+}
 public class BuildableObject : MonoBehaviour, IBuildableObject
 {
     [SerializeField] ReadBuildData ReadBuildData;
+    [SerializeField] BuildUI buildUI;
     public BuildableObjectsData[][] resourceData;
-    public int[] level { get; private set; } = new int[] { 0, 0, 0 };
+    public int[] level { get; private set; } = new int[] { 0, 0, 0,0 };
 
     void Start()
     {
         resourceData = ReadBuildData.GetReadData(); // resourceData[오브젝트타입][레벨]
     }
 
-    public void Build(int type)
-    {
+    public void Build(int type, GameObject buildObject)
+    {        
         bool isNoramlOperation = InventoryManager.Instance.SpendResource(resourceData[type][level[type]].ResourcesName, resourceData[type][level[type]].ResourcesCount);
 
-        if(isNoramlOperation == true) //
+        if(isNoramlOperation == true)
         {
-            gameObject.transform.GetChild(type).gameObject.SetActive(true);
+            buildObject.SetActive(true);
+            if (level[type] < 3)
             level[type]++;
+            buildUI.SetBuildUI();
         }
         else
         {
@@ -26,25 +36,25 @@ public class BuildableObject : MonoBehaviour, IBuildableObject
         }
     }
 
-    public void Upgrade(int type)
-    {
-        if(level[type] == 4)
-        {
-            //만랩
-            return;
-        }
+    //public void Upgrade(int type)
+    //{
+    //    if(level[type] == 4)
+    //    {
+    //        //만랩
+    //        return;
+    //    }
 
-        bool isNoramlOperation = InventoryManager.Instance.SpendResource(resourceData[type][level[type]].ResourcesName, resourceData[type][level[type]].ResourcesCount);
+    //    bool isNoramlOperation = InventoryManager.Instance.SpendResource(resourceData[type][level[type]].ResourcesName, resourceData[type][level[type]].ResourcesCount);
 
-        if (isNoramlOperation == true)
-        {
-            level[type]++;
-        }
-        else
-        {
-            Debug.Log("자원이 부족합니다."); // 추후 UI로 띄우도록 수정
-        }
-    }
+    //    if (isNoramlOperation == true)
+    //    {
+    //        level[type]++;
+    //    }
+    //    else
+    //    {
+    //        Debug.Log("자원이 부족합니다."); // 추후 UI로 띄우도록 수정
+    //    }
+    //}
 
     public void Work()
     {
--- Assets/Scripts/IBuildableObject.cs
+++ Assets/Scripts/IBuildableObject.cs
@@ -1,6 +1,8 @@
+using UnityEngine;
+
 public interface IBuildableObject
 {
-    void Build(int type);
-    void Upgrade(int type);
+    void Build(int type, GameObject buildObject);
+    //void Upgrade(int type);
     void Work();
 }
