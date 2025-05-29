52dcbd072a63a75fdf0027c13ec9eed753be36ea
Yamatchi <ussousso12@gmail.com>
2025-05-29 12:34:27 +0900

    feat : campfire anim 추가
    cooking anim 추가

--- Assets/Scripts/BuildUI.cs
+++ Assets/Scripts/BuildUI.cs
@@ -1,47 +1,47 @@
-using System.Text;
-using TMPro;
-using UnityEngine;
+//using System.Text;
+//using TMPro;
+//using UnityEngine;
 
-public class BuildUI : MonoBehaviour
-{
-    [SerializeField] BuildableObject buildableObject;
-    TextMeshProUGUI[] textUIs;
-    //private void Start()
-    //{
-    //    int BuildableObjCount = gameObject.transform.childCount;
-    //    StringBuilder stringBuilder = new StringBuilder();
+//public class BuildUI : MonoBehaviour
+//{
+//    [SerializeField] BuildableObject buildableObject;
+//    TextMeshProUGUI[] textUIs;
+//    //private void Start()
+//    //{
+//    //    int BuildableObjCount = gameObject.transform.childCount;
+//    //    StringBuilder stringBuilder = new StringBuilder();
 
-    //    for (int i = 0; i < BuildableObjCount; i++)
-    //    {
-    //        int level = buildableObject.level[i];
-    //        for (int j = 0; j < buildableObject.resourceData[i][level].ResourcesName.Length; j++)
-    //        {
-    //            stringBuilder.Append($"{buildableObject.resourceData[i][level].ResourcesName[j]} X {buildableObject.resourceData[i][level].ResourcesCount[j]}\n");
-    //        }
-    //        stringBuilder.Remove(stringBuilder.Length - 1, 1);
+//    //    for (int i = 0; i < BuildableObjCount; i++)
+//    //    {
+//    //        int level = buildableObject.level[i];
+//    //        for (int j = 0; j < buildableObject.resourceData[i][level].ResourcesName.Length; j++)
+//    //        {
+//    //            stringBuilder.Append($"{buildableObject.resourceData[i][level].ResourcesName[j]} X {buildableObject.resourceData[i][level].ResourcesCount[j]}\n");
+//    //        }
+//    //        stringBuilder.Remove(stringBuilder.Length - 1, 1);
 
-    //        string buildText = level == 0 ? "Build" : "Upgrade";
+//    //        string buildText = level == 0 ? "Build" : "Upgrade";
 
-    //        textUIs = gameObject.transform.GetChild(i).GetComponentsInChildren<TextMeshProUGUI>();
-            
-    //        textUIs[0].text = level.ToString();
-    //        textUIs[1].text = stringBuilder.ToString();
-    //        textUIs[2].text = buildText;
-    //    }
-    //}
+//    //        textUIs = gameObject.transform.GetChild(i).GetComponentsInChildren<TextMeshProUGUI>();
 
-    public void BuildButton1()
-    {
-        buildableObject.Build(0);
-    }
+//    //        textUIs[0].text = level.ToString();
+//    //        textUIs[1].text = stringBuilder.ToString();
+//    //        textUIs[2].text = buildText;
+//    //    }
+//    //}
 
-    public void BuildButton2()
-    {
-        buildableObject.Build(1);
-    }
+//    public void BuildButton1()
+//    {
+//        buildableObject.Build(0);
+//    }
 
-    public void BuildButton3()
-    {
-        buildableObject.Build(2);
-    }
-}
+//    public void BuildButton2()
+//    {
+//        buildableObject.Build(1);
+//    }
+
+//    public void BuildButton3()
+//    {
+//        buildableObject.Build(2);
+//    }
+//}
--- Assets/Scripts/Item/Item.cs
+++ Assets/Scripts/Item/Item.cs
@@ -3,13 +3,14 @@ using UnityEngine;
 public class Item : MonoBehaviour
 {
     [SerializeField] private ItemDataSO itemData;
-    public ItemDataSO ItemData => itemData;
-    [SerializeField] private float itemRotate;
+    public ItemDataSO ItemData => itemData; 
+    //[SerializeField] private float itemRotate;
 
-    void Update()
-    {
-        transform.Rotate(Vector3.up * itemRotate * Time.deltaTime);
-    }
+    //void Update()
+    //{
+    //    // item Idle Rotate
+    //    transform.Rotate(Vector3.up * itemRotate * Time.deltaTime);
+    //}
     public void RemoveItem()
     {
         Destroy(this.gameObject);
