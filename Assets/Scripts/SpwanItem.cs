e438a353dc5c02c022ead2b3af0ff8fa72b194e4
Yusang-1 <206505633+Yusang-1@users.noreply.github.com>
2025-05-29 12:00:06 +0900

    feat : 맵 생성 아이템을 빈오브젝트에서 프리펩으로 변경

--- Assets/Scripts/SpwanItem.cs
+++ Assets/Scripts/SpwanItem.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 public class SpwanItem : MonoBehaviour
 {
     [SerializeField] GameObject Item1;
+    [SerializeField] GameObject[] resourcesItem;    
+    [SerializeField] GameObject[] foodItem;
+    [SerializeField] GameObject[] toolItem;
 
     Transform[] spawnAreaArr;
 
@@ -16,15 +19,50 @@ public class SpwanItem : MonoBehaviour
     {
         for (int j = 1; j < spawnAreaArr.Length; j++)
         {
-            for (int i = 0; i < 5; i++)
+            int iNum = Random.Range(3, 5);
+            for (int i = 0; i < iNum; i++)
             {
+                int itemCount = resourcesItem.Length;
+                iNum = Random.Range(0, itemCount);
                 float x = Random.Range(-0.5f, 0.5f);
                 float z = Random.Range(-0.5f, 0.5f);
 
                 Vector3 spawnVec = new Vector3(x, 0, z);
-                Vector3 spawnScale = new Vector3(1 / spawnAreaArr[j].localScale.x, 1, 1 / spawnAreaArr[j].localScale.z);
+                Vector3 spawnScale = new Vector3(4 / spawnAreaArr[j].localScale.x, 4, 4 / spawnAreaArr[j].localScale.z);
 
-                GameObject go = Instantiate(Item1, spawnAreaArr[j]);
+                GameObject go = Instantiate(resourcesItem[iNum], spawnAreaArr[j]);
+                go.transform.localPosition = spawnVec;
+                go.transform.localScale = spawnScale;
+            }
+
+            iNum = Random.Range(1, 2);
+            for (int i = 0; i < iNum; i++)
+            {
+                int itemCount = foodItem.Length;
+                iNum = Random.Range(0, itemCount);
+                float x = Random.Range(-0.5f, 0.5f);
+                float z = Random.Range(-0.5f, 0.5f);
+
+                Vector3 spawnVec = new Vector3(x, 0, z);
+                Vector3 spawnScale = new Vector3(6 / spawnAreaArr[j].localScale.x, 6, 6 / spawnAreaArr[j].localScale.z);
+
+                GameObject go = Instantiate(foodItem[iNum], spawnAreaArr[j]);
+                go.transform.localPosition = spawnVec;
+                go.transform.localScale = spawnScale;
+            }
+
+            iNum = Random.Range(1, 101);
+            if(iNum > 90)
+            {
+                int itemCount = toolItem.Length;
+                iNum = Random.Range(0, itemCount);
+                float x = Random.Range(-0.5f, 0.5f);
+                float z = Random.Range(-0.5f, 0.5f);
+
+                Vector3 spawnVec = new Vector3(x, 0, z);
+                Vector3 spawnScale = new Vector3(6 / spawnAreaArr[j].localScale.x, 6, 6 / spawnAreaArr[j].localScale.z);
+
+                GameObject go = Instantiate(toolItem[iNum], spawnAreaArr[j]);
                 go.transform.localPosition = spawnVec;
                 go.transform.localScale = spawnScale;
             }
