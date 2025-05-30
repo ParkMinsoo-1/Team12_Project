bdbe474b9a19710a3be4e045688b1e1c3f412b09
ParkMinsoo <harimaho88@gmail.com>
2025-05-30 21:21:30 +0900

    edit : Crafting, Interaction 스크립트 수정
    if 문 수정

--- Assets/Scripts/Craft/Crafting.cs
+++ Assets/Scripts/Craft/Crafting.cs
@@ -9,10 +9,7 @@ public class Crafting : MonoBehaviour
     public bool Craft(Recipe recipe)
     {
         if (playerInventory == null)
-        {
             playerInventory = InventoryManager.Instance;
-            
-        }
         
         if (playerInventory.HasResource(recipe.resources))
         {
