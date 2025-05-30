15e1fa4a3376b81dd7d6a3b3c8333c52bf7a0e44
ParkMinsoo <harimaho88@gmail.com>
2025-05-30 15:04:26 +0900

    add : Weapon 스크립트
    무기 데미지 처리 용

---
+++ Assets/Scripts/Player/Weapon.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        //인터페이스를 찾는다
+        //예시로 만들어 놓음.
+        //IDamageable damageable = other.GetComponent<IDamageable>();
+
+
+        //무기의 타입 검출, 무기일 경우, 무기가 가지고 있는 데미지 주기.
+        // if (damageable != null)
+        // {
+        //     damageable.TakeDamage(damage);
+        // }
+    }
+
+}
