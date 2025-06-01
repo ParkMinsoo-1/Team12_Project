3eba31b7f1dc26378b83f9aca4d1765a911f4c21
Yusang-1 <206505633+Yusang-1@users.noreply.github.com>
2025-06-01 21:59:28 +0900

    chore : 제작 기능 중간

--- Assets/Scripts/Craft/Recipe.cs
+++ Assets/Scripts/Craft/Recipe.cs
@@ -5,6 +5,21 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "NewRecipe", menuName = "Craft/Recipe")]
 public class Recipe : ScriptableObject
 {
+    [SerializeField] string craftName;
+    public string CraftName => craftName;
+
+    [SerializeField] string discription;
+    public string Discription => discription;
+
+    [SerializeField] Sprite photo;
+    public Sprite Photo => photo;
+
+    [SerializeField] int jsonDataIndex_1;
+    public int JsonDataIndex_1 => jsonDataIndex_1;
+
+    [SerializeField] int jsonDataIndex_2;
+    public int JsonDataIndex_2 => jsonDataIndex_2;
+
     public List<ItemDataSO> resources;
     public ItemDataSO result;
 }
